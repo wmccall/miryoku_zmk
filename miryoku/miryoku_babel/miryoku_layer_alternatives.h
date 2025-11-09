6247fdcacf5d541d0316c9b594f77e0e20c1f8dd
Will McCall <wnm.code@gmail.com>
2025-11-09 14:22:40 -0700

    Make super/ctrl swap modifications, create wmccall workflow

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -358,13 +358,13 @@ U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp 
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp PLUS,          &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp COLON,         \
 U_NA,              &kp RALT,          &u_to_U_MOUSE,     &u_to_U_SYM,       U_NA,              &kp PIPE,          &kp EXCL,          &kp AT,            &kp HASH,          &kp TILDE,         \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp UNDER,         &kp LPAR,          &kp RPAR,          U_NP,              U_NP
+U_NP,              U_NP,              &tg U_MODSWAP,     U_NA,              U_NA,              &kp UNDER,         &kp LPAR,          &kp RPAR,          U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_SYM \
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
-U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+U_NP,              U_NP,              &tg U_MODSWAP,     U_NA,              U_NA,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NP,              U_NP
 
 
 #define MIRYOKU_ALTERNATIVES_FUN_FLIP \
@@ -385,3 +385,15 @@ U_UND,             U_CUT,             U_CPY,             U_PST,             U_RD
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_MODSWAP_FLIP \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             \
+&kp LCTRL,         &kp LALT,          &kp LGUI,          &kp LSHFT,         U_NA,              &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_NA,              &kp RALT,          &u_to_U_MOUSE,     &u_to_U_SYM,       U_NA,              &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             \
+U_NP,              U_NP,              &tg U_MODSWAP,     U_NA,              U_NA,              &kp H,             &kp J,             &kp K,             U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_MODSWAP \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_NA,              &kp RALT,          &u_to_U_MOUSE,     &u_to_U_SYM,       U_NA,              &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             \
+U_NP,              U_NP,              &tg U_MODSWAP,     U_NA,              U_NA,              &kp H,             &kp J,             &kp K,             U_NP,              U_NP
--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -7,16 +7,17 @@
 #if !defined (MIRYOKU_LAYER_LIST)
 
 #define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun")
+MIRYOKU_X(BASE,    "Base") \
+MIRYOKU_X(EXTRA,   "Extra") \
+MIRYOKU_X(TAP,     "Tap") \
+MIRYOKU_X(BUTTON,  "Button") \
+MIRYOKU_X(NAV,     "Nav") \
+MIRYOKU_X(MOUSE,   "Mouse") \
+MIRYOKU_X(MEDIA,   "Media") \
+MIRYOKU_X(NUM,     "Num") \
+MIRYOKU_X(SYM,     "Sym") \
+MIRYOKU_X(MODSWAP, "ModSwap") \
+MIRYOKU_X(FUN,     "Fun")
 
 #define U_BASE   0
 #define U_EXTRA  1
