a8e7a96449d75ccaa22ddc629fb0d025f7a1fe3f
Will McCall <wnm.code@gmail.com>
2025-11-09 14:29:58 -0700

    Add MODSWAP layer ID

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -16,18 +16,19 @@ MIRYOKU_X(MOUSE,   "Mouse") \
 MIRYOKU_X(MEDIA,   "Media") \
 MIRYOKU_X(NUM,     "Num") \
 MIRYOKU_X(SYM,     "Sym") \
-MIRYOKU_X(MODSWAP, "ModSwap") \
-MIRYOKU_X(FUN,     "Fun")
+MIRYOKU_X(FUN,     "Fun") \
+MIRYOKU_X(MODSWAP, "ModSwap")
 
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
+#define U_BASE    0
+#define U_EXTRA   1
+#define U_TAP     2
+#define U_BUTTON  3
+#define U_NAV     4
+#define U_MOUSE   5
+#define U_MEDIA   6
+#define U_NUM     7
+#define U_SYM     8
+#define U_FUN     9
+#define U_MODSWAP 10
 
 #endif
